877ef5a6dce5d1a3cac3003d93448136260651ac
Nguyễn Minh Hưng <nguyenminhhung05062005@gmail.com>
2026-04-23 08:17:13 +0700

    update photo

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import SectionHeader from '../SectionHeader';
-import myPhoto from '../../assets/photo.jpg';
+import myPhoto from '../../assets/photos.jpg';
 
 const About = () => {
   const skills = [
